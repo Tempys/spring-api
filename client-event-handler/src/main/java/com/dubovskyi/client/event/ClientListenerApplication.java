528e2cb1c29aec78662db673c5ae4e82c8510003
mykhaylo.dubovskyy <Mykhaylo.Dubovskyy@kyivstar.net>
2017-10-09 13:59:49 +0300

    add lookup and Proxy

--- client-event-handler/src/main/java/com/dubovskyi/client/event/ClientListenerApplication.java
+++ client-event-handler/src/main/java/com/dubovskyi/client/event/ClientListenerApplication.java
@@ -9,24 +9,9 @@ import org.springframework.context.ApplicationContext;
 public class ClientListenerApplication {
 
 
-
-/*   static RouterFunction router = route(GET("/"), hello);*/
-
-
-
-
-
-
-
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(ClientListenerApplication.class, args);
 
-     /*   HttpHandler httpHandler = RouterFunctions.toHttpHandler(router);*/
 
-      /*  HttpServer
-                .create("localhost", 8080)
-                .newHandler(new ReactorHttpHandlerAdapter(httpHandler))
-                .block();
-*/
     }
 }
---
+++ spring-cloud-stream-kafka-producer/src/main/java/com.dubovskyi.kafka.stream.producer/ProducerAplication.java
@@ -0,0 +1,19 @@
+package com.dubovskyi.kafka.stream.producer;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class ProducerAplication {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerAplication.class, args);
+
+    }
+
+}
